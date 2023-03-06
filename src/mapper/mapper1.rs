94e25a1a453299b85120d27fc394b7abd61a4ca0
Angus Holder <aholder97@gmail.com>
2023-03-06 13:38:07 +0000

    Further implement mapper #1

--- src/mapper/mapper1.rs
+++ src/mapper/mapper1.rs
@@ -11,11 +11,13 @@ pub struct MMC1Mapper {
     // See https://www.nesdev.org/wiki/MMC1#Registers
     prg_mode: PRGMode,
     chr_mode: CHRMode,
-    chr_bank_0: u32,
-    chr_bank_1: u32,
-    prg_bank: u32,
+    chr_bank_0: u8,
+    chr_bank_1: u8,
+    prg_bank: u8,
 
-    mirroring: NametableMirroring,
+    shift_register: u8,
+
+    mirroring: MMC1Mirroring,
     nametables: [u8; 0x800],
 }
 
@@ -30,6 +32,14 @@ enum PRGMode {
     FixedLastSwitchFirst,
 }
 
+#[derive(Debug)]
+enum MMC1Mirroring {
+    OneScreenLowerBank = 0,
+    OneScreenUpperBank = 1,
+    Vertical = 2,
+    Horizontal = 3,
+}
+
 impl MMC1Mapper {
     pub fn new(cart: Cartridge) -> Self {
         Self {
@@ -39,33 +49,90 @@ impl MMC1Mapper {
             chr_bank_0: 0,
             chr_bank_1: 0,
             prg_bank: 0,
-            mirroring: cart.mirroring,
+            shift_register: 0,
+            mirroring: MMC1Mirroring::OneScreenLowerBank,
             nametables: [0; 0x800],
         }
+    }
+
+    fn write_register(&mut self, addr: u16, value: u8) {
+        if value & 0x80 != 0 {
+            self.reset_shift_register();
+            return;
+        }
+
+        let is_done = self.shift_register & 1 != 0;
+        let mut new_sr = self.shift_register >> 1;
+        if value & 1 != 0 {
+            new_sr |= 0b1_0000;
+        }
+        self.shift_register = new_sr;
+
+        if is_done {
+            match addr & 0xE000 {
+                0x8000 => self.write_control_register(self.shift_register),
+                0xA000 => self.chr_bank_0 = self.shift_register,
+                0xC000 => self.chr_bank_1 = self.shift_register,
+                0xE000 => self.prg_bank = self.shift_register,
+                _ => unreachable!(),
+            }
+            self.reset_shift_register();
+        }
+    }
+
+    fn reset(&mut self) {
+        self.reset_shift_register();
+        // Initially set to PRGMode::FixedLastSwitchFirst
+        self.write_control_register(0xC);
+    }
+
+    fn reset_shift_register(&mut self) {
+        self.shift_register = 0b10000;
+    }
+
+    fn write_control_register(&mut self, byte: u8) {
+        self.mirroring = match byte & 0b11 {
+            0 => MMC1Mirroring::OneScreenLowerBank,
+            1 => MMC1Mirroring::OneScreenUpperBank,
+            2 => MMC1Mirroring::Vertical,
+            3 => MMC1Mirroring::Horizontal,
+            _ => unreachable!(),
+        };
+        self.prg_mode = match byte >> 2 & 0b11 {
+            0 | 1 => PRGMode::Switch32KiB,
+            2 => PRGMode::FixedFirstSwitchLast,
+            3 => PRGMode::FixedLastSwitchFirst,
+            _ => unreachable!(),
+        };
+        self.chr_mode = match byte >> 4 & 1 {
+            0 => CHRMode::Switch8KiB,
+            1 => CHRMode::SwitchTwo4KiB,
+            _ => unreachable!(),
+        };
     }
 }
 
 impl RawMapper for MMC1Mapper {
     fn access_main_bus(&mut self, addr: u16, value: u8, write: bool) -> u8 {
         if write {
-            todo!("MMC1Mapper::access_main_bus write {addr:04X} = {value:02X}")
+            self.write_register(addr, value);
         }
 
         let low_bank: Range<usize>;
         let high_bank: Range<usize>;
         match self.prg_mode {
             PRGMode::Switch32KiB => {
-                let base_addr = (self.prg_bank & !1) as usize;
+                let base_addr = (self.prg_bank & !1) as usize * 16*1024;
                 low_bank = base_addr..base_addr + 16*1024;
                 high_bank = base_addr + 16*1024..base_addr + 32*1024;
             }
             PRGMode::FixedFirstSwitchLast => {
                 low_bank = 0..16*1024;
-                let base_addr = self.prg_bank as usize;
+                let base_addr = self.prg_bank as usize * 16*1024;
                 high_bank = base_addr..base_addr + 16*1024;
             }
             PRGMode::FixedLastSwitchFirst => {
-                let base_addr = self.prg_bank as usize;
+                let base_addr = self.prg_bank as usize * 16*1024;
                 low_bank = base_addr..base_addr + 16*1024;
                 high_bank = self.rom.len() - 16*1024..self.rom.len();
             }
@@ -86,21 +153,29 @@ impl RawMapper for MMC1Mapper {
                 }
                 match self.chr_mode {
                     CHRMode::Switch8KiB => {
-                        let base_addr = (self.chr_bank_0 & !1) as usize;
+                        let base_addr = (self.chr_bank_0 & !1) as usize  * 4 * 1024;
                         self.rom[base_addr + addr as usize]
                     }
                     CHRMode::SwitchTwo4KiB => {
                         let base_addr = if addr < 0x1000 {
-                            self.chr_bank_0 as usize
+                            self.chr_bank_0 as usize * 4 * 1024
                         } else {
-                            self.chr_bank_1 as usize
+                            self.chr_bank_1 as usize * 4 * 1024
                         };
                         self.rom[base_addr + addr as usize]
                     }
                 }
             },
             0x2000..=0x2FFF | 0x3000..=0x3EFF => {
-                let ptr = access_nametable(&mut self.nametables, self.mirroring, addr & 0x2FFF);
+                let nt_mirroring = match self.mirroring {
+                    MMC1Mirroring::Horizontal => NametableMirroring::Horizontal,
+                    MMC1Mirroring::Vertical => NametableMirroring::Vertical,
+                    _ => {
+                        warn!("Unimplemented mirroring mode {:?}, defaulting to horizontal", self.mirroring);
+                        NametableMirroring::Horizontal
+                    }
+                };
+                let ptr = access_nametable(&mut self.nametables, nt_mirroring, addr & 0x2FFF);
                 if write {
                     *ptr = value;
                 }
