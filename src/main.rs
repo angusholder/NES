717eb0c9f39f5ac9da34e95af60698f8d1131ac2
Angus Holder <aholder97@gmail.com>
2023-03-07 23:35:48 +0000

    Add frame time stats into the window title bar

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod mapper;
 mod disassemble;
 mod input;
 
+use std::collections::VecDeque;
 use std::error::Error;
 use std::io::Write;
 use std::panic::catch_unwind;
@@ -19,7 +20,7 @@ use sdl2::pixels::{Color, Palette, PixelFormatEnum};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use sdl2::messagebox::{ButtonData, MessageBoxButtonFlag, MessageBoxFlag, show_message_box};
 use sdl2::surface::Surface;
 use crate::mapper::Mapper;
@@ -56,7 +57,7 @@ fn main_loop() -> Result<(), Box<dyn Error>> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("NES Emulator", SCREEN_WIDTH*2, SCREEN_HEIGHT*2)
+    let mut window = video_subsystem.window("NES Emulator", SCREEN_WIDTH*2, SCREEN_HEIGHT*2)
         .position_centered()
         .build()?;
 
@@ -64,9 +65,11 @@ fn main_loop() -> Result<(), Box<dyn Error>> {
     display_buffer_paletted.set_palette(&load_nes_palette())?;
     let mut display_buffer_rgb = Surface::new(SCREEN_WIDTH, SCREEN_HEIGHT, PixelFormatEnum::ARGB8888)?;
 
+    let mut frame_stats = FrameStats::new();
     let mut event_pump = sdl_context.event_pump()?;
     let mut nes: Option<NES> = None;
     'running: loop {
+        let start_time = Instant::now();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -97,7 +100,10 @@ fn main_loop() -> Result<(), Box<dyn Error>> {
         display_buffer_rgb.blit_scaled(None, &mut window_surf, None)?;
         window_surf.finish()?;
 
-        thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        window.set_title(&format!("NES Emulator - {:.2}ms", frame_stats.get_avg_frame_time_ms()))?;
+        let frame_time = start_time.elapsed();
+        frame_stats.add_reading(frame_time);
+        thread::sleep(Duration::new(0, 1_000_000_000u32 / 60).saturating_sub(frame_time));
     }
 
     Ok(())
@@ -138,4 +144,35 @@ fn display_error_dialog(title: &str, message: &str) {
         message,
         None, None,
     ).unwrap();
+}
+
+struct FrameStats {
+    frame_count: u32,
+    frame_times: VecDeque<Duration>,
+}
+
+const MAX_READINGS: usize = 60;
+
+impl FrameStats {
+    fn new() -> FrameStats {
+        FrameStats {
+            frame_count: 0,
+            frame_times: VecDeque::with_capacity(MAX_READINGS),
+        }
+    }
+
+    fn add_reading(&mut self, time: Duration) {
+        self.frame_count += 1;
+        self.frame_times.truncate(MAX_READINGS - 1);
+        self.frame_times.push_back(time);
+    }
+
+    fn get_avg_frame_time_ms(&self) -> f64 {
+        if self.frame_times.is_empty() {
+            return 0.0;
+        }
+        let total_time: Duration = self.frame_times.iter().sum();
+        let mean = total_time / self.frame_times.len() as u32;
+        mean.as_micros() as f64 / 1000.0
+    }
 }
