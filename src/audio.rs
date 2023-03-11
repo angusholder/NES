03008b4d91868a00d1e4763a4f9d245b7f844110
Angus Holder <aholder97@gmail.com>
2023-03-11 23:56:22 +0000

    Add square wave 2, and obey the APU control bits and square duty cycle

--- src/audio.rs
+++ src/audio.rs
@@ -6,6 +6,14 @@ use sdl2::audio::{AudioCallback, AudioDevice, AudioSpec, AudioSpecDesired};
 pub struct APU {
     output_buffer: SampleBuffer,
     square_wave1: SquareWave,
+    square_wave2: SquareWave,
+
+    enable_square1: bool,
+    enable_square2: bool,
+    enable_triangle: bool,
+    enable_noise: bool,
+    enable_dmc: bool,
+
     last_cpu_cycles: u64,
 }
 
@@ -62,6 +70,14 @@ impl APU {
         APU {
             output_buffer: SampleBuffer::new(),
             square_wave1: SquareWave::new(AUDIO_FREQUENCY),
+            square_wave2: SquareWave::new(AUDIO_FREQUENCY),
+
+            enable_square1: false,
+            enable_square2: false,
+            enable_triangle: false,
+            enable_noise: false,
+            enable_dmc: false,
+
             last_cpu_cycles: 0,
         }
     }
@@ -72,7 +88,17 @@ impl APU {
 
     pub fn run_until_cycle(&mut self, cpu_cycle: u64) {
         let start_cycle = self.last_cpu_cycles;
-        let samples = self.square_wave1.output_samples(start_cycle, cpu_cycle);
+        let mut samples = self.square_wave1.output_samples(start_cycle, cpu_cycle);
+        if !self.enable_square1 {
+            samples.fill(0.0);
+        }
+        let samples2 = self.square_wave2.output_samples(start_cycle, cpu_cycle);
+        if self.enable_square2 {
+            for (s0, s1) in samples.iter_mut().zip(samples2.iter()) {
+                *s0 += *s1;
+            }
+        }
+
         self.output_buffer.write_samples(&samples);
 
         self.last_cpu_cycles = cpu_cycle;
@@ -82,14 +108,24 @@ impl APU {
         self.run_until_cycle(cpu_cycle);
 
         match addr {
-            0x4002 => {
-                self.square_wave1.period = self.square_wave1.period & 0xFF00 | value as u32;
+            0x4000 => self.square_wave1.write_control(value),
+            0x4001 => self.square_wave1.write_ramp(value),
+            0x4002 => self.square_wave1.write_fine_tune(value),
+            0x4003 => self.square_wave1.write_coarse_tune(value),
+
+            0x4004 => self.square_wave2.write_control(value),
+            0x4005 => self.square_wave2.write_ramp(value),
+            0x4006 => self.square_wave2.write_fine_tune(value),
+            0x4007 => self.square_wave2.write_coarse_tune(value),
+
+            0x4015 => {
+                self.enable_square1 = (value & 0x01) != 0;
+                self.enable_square2 = (value & 0x02) != 0;
+                self.enable_triangle = (value & 0x04) != 0;
+                self.enable_noise = (value & 0x08) != 0;
+                self.enable_dmc = (value & 0x10) != 0;
             }
-            0x4003 => {
-                self.square_wave1.phase = 0.0;
-                self.square_wave1.period = self.square_wave1.period & 0x00FF | ((value as u32 & 0x7) << 8);
-                // TODO: Reset length counter
-            }
+
             _ => {}
         }
     }
@@ -154,6 +190,34 @@ impl SquareWave {
             self.volume,
             self.duty_cycle,
         )
+    }
+
+    // $4003/$4007
+    fn write_coarse_tune(&mut self, value: u8) {
+        self.phase = 0.0;
+        self.period = self.period & 0x00FF | ((value as u32 & 0x7) << 8);
+        // TODO: Reset length counter
+    }
+
+    // $4002/$4006
+    fn write_fine_tune(&mut self, value: u8) {
+        self.period = self.period & 0xFF00 | value as u32;
+    }
+
+    // $4000/$4004
+    fn write_control(&mut self, value: u8) {
+        self.duty_cycle = match value >> 6 {
+            0 => 0.125,
+            1 => 0.25,
+            2 => 0.5,
+            3 => 0.75,
+            _ => unreachable!(),
+        };
+    }
+
+    // $4001/$4005
+    fn write_ramp(&mut self, _value: u8) {
+
     }
 }
 
