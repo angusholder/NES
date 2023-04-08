angusholder/NES

commit cf69199b3a3d8e1a3f43bfdadd1fcb36fe72b1a1

Split Input::handle_register_access() into read/write methods

nes_core/src/nes.rs
@@ -213,7 +213,7 @@ impl NES {
         } else if addr < 0x4000 {
             return ppu::ppu_read_register(&mut self.ppu, addr);
         } else if addr == input::JOYPAD_1 || addr == input::JOYPAD_2 {
-            return self.input.handle_register_access(addr, 0, false);
+            return self.input.read_register(addr);
         } else if addr < 0x4020 {
             // TODO: Implement APU memory reads
             return 0;
@@ -247,7 +247,7 @@ impl NES {
         } else if addr < 0x4000 {
             ppu::ppu_write_register(&mut self.ppu, addr, val);
         } else if addr == input::JOYPAD_1 || addr == input::JOYPAD_2 {
-            self.input.handle_register_access(addr, val, true);
+            self.input.write_register(addr, val);
         } else if addr == 0x4014 {
             ppu::do_oam_dma(self, val);
         } else if addr < 0x4020 {

nes_core/src/input.rs
@@ -27,33 +27,38 @@ impl InputState {
         }
     }
 
-    pub fn handle_register_access(&mut self, addr: u16, val: u8, write: bool) -> u8 {
-        if write && addr == JOYPAD_1 {
-            if val & 1 != 0 {
-                self.is_polling = true;
-            } else {
-                if self.is_polling {
-                    self.is_polling = false;
-                    self.joypad1_shift_register = self.pressed.bits;
+    pub fn write_register(&mut self, addr: u16, val: u8) {
+        match addr {
+            JOYPAD_1 => {
+                if val & 1 != 0 {
+                    self.is_polling = true;
+                } else {
+                    if self.is_polling {
+                        self.is_polling = false;
+                        self.joypad1_shift_register = self.pressed.bits;
+                    }
                 }
             }
-            return 0;
+            JOYPAD_2 => {
+                // Ignored
+            }
+            _ => mapper::out_of_bounds_write("INPUT", addr, val),
         }
-        if write && addr == JOYPAD_2 {
-            // Ignore
-            return 0;
-        }
-        if !write && addr == JOYPAD_1 {
-            let next_bit = self.joypad1_shift_register & 1;
-            self.joypad1_shift_register >>= 1;
-            return next_bit;
-        }
-        if !write && addr == JOYPAD_2 {
-            // Controller 2 is not implemented, always return 0
-            return 0;
-        }
+    }
 
-        mapper::out_of_bounds_access("INPUT", addr, val, write)
+    pub fn read_register(&mut self, addr: u16) -> u8 {
+        match addr {
+            JOYPAD_1 => {
+                let next_bit = self.joypad1_shift_register & 1;
+                self.joypad1_shift_register >>= 1;
+                next_bit
+            }
+            JOYPAD_2 => {
+                // Controller 2 is not implemented, always return 0
+                0
+            }
+            _ => mapper::out_of_bounds_read("INPUT", addr),
+        }
     }
 }
 

nes_core/src/mapper.rs
@@ -219,15 +219,3 @@ pub fn out_of_bounds_read(context: &str, addr: u16) -> u8 {
 pub fn out_of_bounds_write(context: &str, addr: u16, value: u8) {
     log::warn!("Attempted to write {context} out of bounds at {addr:04X} with {value} (0x{value:02X})");
 }
-
-#[inline(never)]
-#[track_caller]
-#[cold]
-pub fn out_of_bounds_access(context: &str, addr: u16, value: u8, write: bool) -> u8 {
-    if write {
-        out_of_bounds_write(context, addr, value);
-        return 0;
-    } else {
-        out_of_bounds_read(context, addr)
-    }
-}

commit 80ee627d6444bdf16ccd1178769ea8808589d2d8

Add back in missing PPU OAM memory op
Broken in cf69199b3a3d8e1a3f43bfdadd1fcb36fe72b1a1

nes_core/src/nes.rs
@@ -285,6 +285,8 @@ impl NES {
                 ppu::ppu_write_register(&mut self.ppu, addr, val),
             JOYPAD_1 | JOYPAD_2 =>
                 self.input.write_register(addr, val),
+            0x4014 =>
+                ppu::do_oam_dma(self, val),
             0x4000..=0x401F =>
                 self.apu.write_register(addr, val, self.total_cycles),
         }